a7a710e50fd222ae4504bc6ebaacd3760ff6d1b9
Slade <saib6064@gmail.com>
2026-03-05 01:31:09 +0700

    fix(next): fix css bug and path error

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
+import path from "node:path";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  outputFileTracingRoot: path.join(__dirname, "../.."),
 };
 
 export default nextConfig;
